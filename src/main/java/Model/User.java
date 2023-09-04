13c2a97c14efea64f4d449a09fa98942052f1a59
Larin <lagoss@yandex.ru>
2023-09-04 20:09:28 +0300

    related

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -8,80 +8,77 @@ import java.util.List;
  * @apiNote Класс пользователя: хранит пароль, счетa, имя
  */
 public class User implements Serializable {
-	/**
-	 * @return Пароль пользователя
-	 */
-	private String password;
-	/**
-	 * @apiNote Имя пользователя
-	 */
-	private final String name;
+    /**
+     * @return Пароль пользователя
+     */
+    private String password;
+    /**
+     * @apiNote Имя пользователя
+     */
+    private final String name;
 
-	private List<Account> listAccounts = new ArrayList<Account>();
+    private List<Account> listAccounts = new ArrayList<Account>();
 
-	/**
-	 * @apiNote Конструктор для создания юзера
-	 * @param 1 строка name - Имя пользователя
-	 * @param 2 строка password - пароль пользователя
-	 */
-	public User(String name, String password) {
-		this.name = name;
-		this.password = password;
-	}
+    /**
+     * @param name     Имя пользователя
+     * @param password пароль пользователя
+     * @apiNote Конструктор для создания юзера
+     */
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 
-	/**
-	 * 
-	 * @return String - имя пользователя
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * @return String - имя пользователя
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @return int - хешкод пароля пользователя
-	 */
-	public int getPassword() {
-		return this.password.hashCode();
-	}
+    /**
+     * @return int - хешкод пароля пользователя
+     */
+    public int getPassword() {
+        return this.password.hashCode();
+    }
 
-	/**
-	 * @apiNote Метод изменения пароля
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * @apiNote Метод изменения пароля
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	/**
-	 * 
-	 * @param account - счет для добавления в список
-	 * @return
-	 */
-	public boolean addAccount(Account account) {
-		return listAccounts.add(account);
-	}
+    /**
+     * @param account - счет для добавления в список
+     * @return
+     */
+    public boolean addAccount(Account account) {
+        return listAccounts.add(account);
+    }
 
-	/**
-	 * 
-	 * @param account - какой счет нужно удалить
-	 * @return
-	 */
-	public boolean removeAccount(Account account) {
-		return listAccounts.remove(account);
-	}
+    /**
+     * @param account - какой счет нужно удалить
+     * @return
+     */
+    public boolean removeAccount(Account account) {
+        return listAccounts.remove(account);
+    }
 
-	public List<Account> getListAccounts() {
-		return listAccounts;
-	}
+    public List<Account> getListAccounts() {
+        return listAccounts;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(name);
-		for (Account a : listAccounts) {
-			sb.append("\n"+a);
-		}
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        for (Account a : listAccounts) {
+            sb.append("\n" + a);
+        }
+        return sb.toString();
+    }
 }
 
-}
+
